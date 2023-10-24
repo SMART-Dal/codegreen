2878279fbe745c4f514f777f7f115b52653cd0d1
Saurabhsingh Rajput <saurabh@dal.ca>
2023-10-24 17:27:58 -0300

    fix symlink

--- codegreen/main.py
+++ codegreen/main.py
@@ -10,47 +10,16 @@ import shutil
 import subprocess
 from codegreen.fecom.patching.patching_config import METHOD_LEVEL_PATCHING_SCRIPT_PATH
 from codegreen.fecom.patching.repo_patching import patch_project
-import git
+from codegreen.utils.metadata import get_repo_metadata
+from codegreen import __version__
 import os
 
 app = typer.Typer(rich_markup_mode="rich",help="[green]🍃 CodeGreen: Your Passport to a Greener Code! 🍃[/green]")
 
-
-def get_repo_metadata(repo_dir):
-  metadata = {}
-  
-  try:
-
-    repo = git.Repo(repo_dir)
-
-    # Project name
-    metadata["project_name"] = os.path.basename(repo_dir)
-
-    # Project repository 
-    metadata["project_repository"] = repo.remote().url
-
-    # Project owner
-    metadata["project_owner"] = repo.remote().url.split("/")[-2]
-
-    # Project branch
-    metadata["project_branch"] = repo.active_branch.name
-
-    # Project commit
-    metadata["project_commit"] = repo.head.commit.hexsha
-
-    # Project commit date
-    metadata["project_commit_date"] = repo.head.commit.committed_datetime.isoformat()
-
-    # Script path - would need to pass this explicitly
-    metadata["script_path"] = None
-
-    # API call line - would need to parse files to get this
-    metadata["api_call_line"] = None
-
-  except git.InvalidGitRepositoryError:
-    print(f"{repo_dir} is not a Git repository, so git metadata cannot be retrieved.")
-
-  return metadata
+@app.callback()
+def version():
+    print(f"Awesome CLI Version: {__version__}")
+    raise typer.Exit()
 
 def get_script_path(script_name, method_level_python_scripts):
     for script_path in method_level_python_scripts:
---
+++ codegreen/utils/metadata.py
@@ -0,0 +1,37 @@
+import git
+
+def get_repo_metadata(repo_dir):
+  metadata = {}
+  
+  try:
+
+    repo = git.Repo(repo_dir)
+
+    # Project name
+    metadata["project_name"] = os.path.basename(repo_dir)
+
+    # Project repository 
+    metadata["project_repository"] = repo.remote().url
+
+    # Project owner
+    metadata["project_owner"] = repo.remote().url.split("/")[-2]
+
+    # Project branch
+    metadata["project_branch"] = repo.active_branch.name
+
+    # Project commit
+    metadata["project_commit"] = repo.head.commit.hexsha
+
+    # Project commit date
+    metadata["project_commit_date"] = repo.head.commit.committed_datetime.isoformat()
+
+    # Script path - would need to pass this explicitly
+    metadata["script_path"] = None
+
+    # API call line - would need to parse files to get this
+    metadata["api_call_line"] = None
+
+  except git.InvalidGitRepositoryError:
+    print(f"{repo_dir} is not a Git repository, so git metadata cannot be retrieved.")
+
+  return metadata
