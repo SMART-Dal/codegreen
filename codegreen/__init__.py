4449ff3e885f02c63101619d6f54b81a38521789
Saurabh <saurabhsinghkrajput@gmail.com>
2026-03-31 03:05:54 -0300

    v0.3.4: fix cmake install prefix (use .. relative to build dir, not {project} which resolves early)

--- codegreen/__init__.py
+++ codegreen/__init__.py
@@ -5,7 +5,7 @@ A comprehensive energy measurement and code optimization tool for developers
 and researchers who need precise, fine-grained energy consumption analysis.
 """
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __author__ = "Saurabhsingh Rajput"
 __email__ = "saurabh@dal.ca"
 __description__ = "Energy-aware software development tool"
